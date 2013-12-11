7b8786e095f903d2de529137fe0869b3f1339ae2
E.Azer Koçulu <azer@kodfabrik.com>
2013-12-10 17:55:51 -0800

    Start capitalized after special characters

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var anglicize = require("anglicize");
 module.exports = variableName;
 
 function variableName (str) {
-  return anglicize(str).toLowerCase().replace(/[^a-zA-Z0-9\s]+/g,'').replace(/^[\d\s]+/g,'').split(' ').reduce(function(a,b){
+  return anglicize(str).toLowerCase().replace(/[^a-zA-Z0-9\s]+/g,' ').replace(/^[\d\s]+/g,'').split(' ').reduce(function (a,b) {
     return a + b.charAt(0).toUpperCase() + b.slice(1).toLowerCase();
   });
 }
