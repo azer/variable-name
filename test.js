9d1ded9624f9bf8a09d63ffbd98a4107a011c206
E.Azer Koçulu <azer@kodfabrik.com>
2015-09-27 13:49:04 -0700

    clean quotes

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 var variableName = require("./");
+var test = require("prova");
 
-it('generates a legal variable name from string', function(){
-  expect(variableName('azer koçulu')).to.equal('azerKoculu');
-  expect(variableName('hello world ÂÇİĞÖŞÜÑ bar!')).to.equal('helloWorldAcigosunBar');
+test('generates a legal variable name from string', function (t) {
+  t.plan(4);
+  t.equal(variableName('azer koçulu'), 'azerKoculu');
+  t.equal(variableName('hello world ÂÇİĞÖŞÜÑ bar!'), 'helloWorldAcigosunBar');
+  t.equal(variableName("azer's new stickers"), 'azersNewStickers');
+  t.equal(variableName("it's `\"allright\"`"), 'itsAllright');
 });
